754cb7dcf8e716bd6230a589652070e49f6e2b82
Melrizzuto <melrizzuto4@gmail.com>
2024-11-11 14:52:48 +0100

    added folders and exercise js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* Consegna
+
+1. Visualizzare in pagina 5 numeri casuali. 
+
+2. Da lì parte un timer di 30 secondi.
+
+3. Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, 
+nell'ordine che preferisce. 
+(Per favorire il lavoro vostro e dei tutor mettete inizialmente un timer di 5-10 sec e non 30)
+
+4. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
